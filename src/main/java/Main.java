bba9a05453830fc511f31e5b9839e4f8dc26322c
Matias Scandroglio <matias.scandroglio@almundo.com>
2018-05-12 20:41:08 -0300

    update main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,8 @@
-import dao.ClienteDAO;
-import model.Cliente;
+import server.Servidor;
 
 public class Main {
 
     public static void main(String[] args) {
-        Cliente cliente = new Cliente(123456789,
-                "pepe",
-                "argento",
-                "la concha 123",
-                "20-35156545-5",
-                "queVaAca",
-                50000,
-                25000);
-
-        //ClienteDAO.save(cliente);
-        ClienteDAO.getCliente(33545334);
-
+        new Servidor();
     }
 }
