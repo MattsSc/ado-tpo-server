78a094ab39be246234f6a28a41e70243b8fe0c7f
matias.scandroglio <matias.scan@gmail.com>
2018-07-02 19:39:35 -0300

    sajsaklf

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -57,9 +57,45 @@ public class Main {
                 3000
         );
 
+        Articulo noStock1 = new Articulo(
+                4444,
+                "NoStock",
+                TipoProducto.BOLSA.name(),
+                1,
+                1,
+                15f,
+                3000
+        );
+
+        Articulo noStock2 = new Articulo(
+                5555,
+                "NoStock2",
+                TipoProducto.BOLSA.name(),
+                1,
+                1,
+                15f,
+                3000
+        );
+
+        Articulo stock800 = new Articulo(
+                6666,
+                "stock800",
+                TipoProducto.BOLSA.name(),
+                1,
+                1,
+                15f,
+                800
+        );
+
+
+
         coca.save();
         pepsi.save();
         cheetos.save();
+        noStock1.save();
+        noStock2.save();
+        stock800.save();
+
         cliente.save();
 
         Proveedor proovedor = new Proveedor("Potigian S.A", 21134504);
@@ -96,15 +132,19 @@ public class Main {
         ordenCheetos1.save();
         OrdenDeCompra ordenCoca2 = new OrdenDeCompra(coca,proovedor2);
         ordenCoca2.save();
+        OrdenDeCompra ordenSotck800 = new OrdenDeCompra(stock800,proovedor3);
+        ordenSotck800.save();
+
 
         try {
             DateFormat sourceFormat = new SimpleDateFormat("dd/MM/yyyy");
-            Date fecha1 = sourceFormat.parse("02/07/2018");
+            Date fecha1 = sourceFormat.parse("02/09/2018");
             Date fecha2 = sourceFormat.parse("02/08/2018");
             ControladorCompra.getInstance().cerrarOrdenDeCompra(ordenCoca1.getId(), 40000f,fecha2);
             ControladorCompra.getInstance().cerrarOrdenDeCompra(ordenCoca2.getId(), 35000f, fecha1);
             ControladorCompra.getInstance().cerrarOrdenDeCompra(ordenPepsi1.getId(), 37000f, fecha1);
             ControladorCompra.getInstance().cerrarOrdenDeCompra(ordenCheetos1.getId(), 25000f, fecha1);
+            ControladorCompra.getInstance().cerrarOrdenDeCompra(ordenSotck800.getId(), 10000f, fecha2);
         } catch (RemoteException e) {
             e.printStackTrace();
         } catch (ParseException e) {
